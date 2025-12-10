4537c1522e4c506501ac8c0a717eb16b08d84d8b
CerritosCodeNinjas <127266053+CerritosCodeNinjas@users.noreply.github.com>
2025-12-10 13:35:26 -0800

    Update config.js

--- config.js
+++ config.js
@@ -1,14 +1,15 @@
 // config.js
 const APP_VERSION = "8.2";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyAElu-JLX7yAJJ4vEnR4SMZGn0zf93KvCQ",
-  authDomain: "codeninjas-dashboard.firebaseapp.com",
-  projectId: "codeninjas-dashboard",
-  storageBucket: "codeninjas-dashboard.firebasestorage.app",
-  messagingSenderId: "71590347120",
-  appId: "1:71590347120:web:5f53a55cd7ffc280fd8fb5"
-};
+ const firebaseConfig = {
+    apiKey: "AIzaSyCLHWkqGBOf-JBrFT0QniaDmbX0FyBO44I",
+    authDomain: "dojocatalog.firebaseapp.com",
+    projectId: "dojocatalog",
+    storageBucket: "dojocatalog.firebasestorage.app",
+    messagingSenderId: "962932301936",
+    appId: "1:962932301936:web:183601f8005e1a7fb814b5",
+    measurementId: "G-CLL6HXFTX4"
+  };
 
 const GITHUB_REPO_URL = "https://github.com/YOUR_USERNAME/YOUR_REPO_NAME"; 
 
@@ -28,4 +29,5 @@ const defaultNews = [{ id: "n1", title: "Minecraft Night", date: "Nov 22", badge
 const defaultRules = [{ id: "r1", title: "General", desc: "Respect the Dojo equipment.", penalty: "-1 Coin" }];
 const defaultCoins = [{ id: "c1", task: "Wear Uniform", val: "+1", type: "silver" }];
 const defaultCatalog = [{ id: "cat1", name: "Star", cost: "50", tier: "tier1", category: "standard", icon: "fa-star", visible: true }];
+
 const mockLeaderboard = [{ id: "l1", name: "Asher C.", points: 1250, belt: "Blue", username: "asher.cullin" }];
